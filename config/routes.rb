3a64be013ec7f5b59f2cbe3b1fb53d7e7df00bc8
Advit <advitgandhi12@gmail.com>
2023-01-04 17:02:14 +0530

    Added validation and flash messages

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
 
   get "about", to:"pages#about"
   
-  resources :articles, only: [:show]
+  resources :articles, only: [:show, :index, :create, :new]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
