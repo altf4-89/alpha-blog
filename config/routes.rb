ecde4884ca2a5cab39f9f531f69b2072ed63cac5
Advit <advitgandhi12@gmail.com>
2023-01-11 16:15:31 +0530

    Added user sign up functionality

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
 
   get "about", to:"pages#about"
   
+  get "signup", to:"users#new"
+  resources :users, except: [:new]
   resources :articles
   # , only: [:show, :index, :create, :new, :edit, :update, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
